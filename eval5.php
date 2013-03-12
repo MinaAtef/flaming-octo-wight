a6c5dcac96f18ccb6d7a86a2c281c5277505900b
IT <IT@IT-PC>
2013-03-12 23:29:40 +0200

    Done1

---
+++ eval5.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<html>
+    <body>
+
+        <form action="" method="post">
+            <h2> Course: </h2>
+            Name : <select name="name"></select> 
+            <br><br>
+            Code : <input type="text" name="code">
+            <br><br>
+            <input type="button" name="addedit" value="Add/Edit">
+            <input type="button" name="Delete" value="Delete">
+        </form>
+        
+    </body>
+</html>
